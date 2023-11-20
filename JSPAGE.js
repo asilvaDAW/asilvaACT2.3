43274ddb78cb4f4f546ca91adde8de384db09cd5
alba <alba@abastos.org>
2023-11-20 13:58:41 +0100

    corrigiendo la etiqueta

--- JSPAGE.js
+++ JSPAGE.js
@@ -3,11 +3,11 @@ function cambiacolor(){
     color = document.getElementById("colorfondo").innerHTML;
     if (color == "azul") {
 
-        document.getElementsByTagName("body")[0].style.background = "red";
+        document.getElementsByTagName("body")[0].style.backgroundColor = "red";
         color = document.getElementById("colorfondo").innerHTML = "rojo";
     }
     if (color == "rojo") {
-        document.getElementsByTagName("body")[0].style.background = "rgb(53, 107, 109)";
+        document.getElementsByTagName("body")[0].style.backgroundColor = "rgb(53, 107, 109)";
         color = document.getElementById("colorfondo").innerHTML = "azul";
     }
 }

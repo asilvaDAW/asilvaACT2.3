a850147231f02e4c28acc82f480404e0f6eb919d
alba <alba@abastos.org>
2023-11-20 13:57:57 +0100

    sorrigiendo

--- JSPAGE.js
+++ JSPAGE.js
@@ -1,13 +1,13 @@
 function cambiacolor(){
     debugger;
-    color = document.getElementById("colorfondo").innerText;
+    color = document.getElementById("colorfondo").innerHTML;
     if (color == "azul") {
 
         document.getElementsByTagName("body")[0].style.background = "red";
-        color = document.getElementById("colorfondo").innerText = "rojo";
+        color = document.getElementById("colorfondo").innerHTML = "rojo";
     }
     if (color == "rojo") {
         document.getElementsByTagName("body")[0].style.background = "rgb(53, 107, 109)";
-        color = document.getElementById("colorfondo").innerText = "azul";
+        color = document.getElementById("colorfondo").innerHTML = "azul";
     }
 }
